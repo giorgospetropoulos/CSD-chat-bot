45f80fca498b3136aaf44ca704126eefadd0bf60
giorgosxp <32841218+giorgospetropoulos@users.noreply.github.com>
2020-09-29 20:11:53 +0300

    Update TeachingCourses Activity
    added slide menu

--- app/src/main/java/com/example/csdbot/activities/TeachingCoursesActivity.java
+++ app/src/main/java/com/example/csdbot/activities/TeachingCoursesActivity.java
@@ -1,11 +1,16 @@
 package com.example.csdbot.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,6 +24,7 @@ import com.example.csdbot.components.Course;
 import com.example.csdbot.components.DatabaseHelper;
 import com.example.csdbot.components.PostGraduateCourse;
 import com.example.csdbot.components.User;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,7 +34,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class TeachingCoursesActivity extends AppCompatActivity {
+public class TeachingCoursesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ArrayList<Course> undergraduateCoursesList = new ArrayList<Course>();
     private ArrayAdapter<Course> undergraduateCoursesAdapter;
@@ -39,6 +45,12 @@ public class TeachingCoursesActivity extends AppCompatActivity {
     private User user;
     private ProgressDialog loading;
 
+    // ---------- Slide Menu --------------
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    // ------------------------------------
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +58,19 @@ public class TeachingCoursesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_teaching_courses);
         loading = ProgressDialog.show(TeachingCoursesActivity.this, "",
                 "Loading. Please wait...", true);
+
+        // ---------- Slide Menu --------------
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Teaching Courses");
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        // ------------------------------------
 
         /* Find the activity's views
          *      undergraduateCoursesBtn: Undergraduate Courses Button
@@ -123,10 +148,56 @@ public class TeachingCoursesActivity extends AppCompatActivity {
         postgraduateCoursesLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(TeachingCoursesActivity.this, CoursePageActivity.class);
+                Intent intent = new Intent(TeachingCoursesActivity.this, PostGraduateCourseActivity.class);
                 intent.putExtra("Course Name", postgraduateCoursesList.get(position).getName_en());
                 startActivity(intent);
             }
         });
     }
+
+
+    // ----------------------------- Menu Functions --------------------------------------
+    @Override
+    public void onBackPressed() {
+        if ( drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                Intent intent_home = new Intent(TeachingCoursesActivity.this, HomeActivity.class);
+                startActivity(intent_home);
+                return true;
+            case R.id.nav_profile:
+                Intent intent_profile = new Intent(TeachingCoursesActivity.this, ProfileActivity.class);
+                startActivity(intent_profile);
+                return true;
+            case R.id.nav_courses:
+                Intent intent_courses = new Intent(TeachingCoursesActivity.this, MyCoursesActivity.class);
+                startActivity(intent_courses);
+                return true;
+            case R.id.nav_reminders:
+                Intent intent_reminders = new Intent(TeachingCoursesActivity.this, RemindersListActivity.class);
+                startActivity(intent_reminders);
+                return true;
+            case R.id.nav_contacts:
+                Intent intent_contacts = new Intent(TeachingCoursesActivity.this, ContactsActivity.class);
+                startActivity(intent_contacts);
+                return true;
+            case R.id.nav_log_out:
+                firebaseAuth.signOut();
+                finish();
+                Intent returnToSignUp = new Intent(TeachingCoursesActivity.this, MainActivity.class);
+                startActivity(returnToSignUp);
+                return true;
+            default:
+                return false;
+        }
+    }
+    // --------------------------------------------------------------------------------------------
 }
